ed119473208cbfef0668f163092d9f2df0a49772
Yulissa_Hrnndz <140362195+Yulissa20220818@users.noreply.github.com>
2024-06-06 20:20:23 -0600

    Add files via upload

---
+++ Parcial_3/Parcial_3/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Media;
+
+class AdivinaElNumero
+{
+    static void Main(string[] args)
+    {
+        Random random = new Random();
+        int numeroSecreto = random.Next(1, 101);
+        int intentos = 0;
+        bool adivinado = false;
+
+        Console.ForegroundColor = ConsoleColor.Cyan;
+        Console.WriteLine("¡Bienvenido al juego de Adivina el Número!");
+        Console.ResetColor();
+
+        while (!adivinado)
+        {
+            intentos++;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write("Adivina el número (entre 1 y 100): ");
+            Console.ResetColor();
+
+            string input = Console.ReadLine();
+            int numeroUsuario;
+
+            if (int.TryParse(input, out numeroUsuario))
+            {
+                if (numeroUsuario == numeroSecreto)
+                {
+                    adivinado = true;
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("¡Felicidades! Adivinaste el número en {0} intentos.", intentos);
+                    Console.ResetColor();
+                    PlaySound("win.wav");
+                }
+                else if (numeroUsuario < numeroSecreto)
+                {
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.WriteLine("El número secreto es mayor. Intenta nuevamente.");
+                    Console.ResetColor();
+                    PlaySound("wrong.wav");
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("El número secreto es menor. Intenta nuevamente.");
+                    Console.ResetColor();
+                    PlaySound("wrong.wav");
+                }
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.WriteLine("Entrada no válida. Por favor ingresa un número.");
+                Console.ResetColor();
+                PlaySound("error.wav");
+            }
+        }
+
+        Console.ForegroundColor = ConsoleColor.Cyan;
+        Console.WriteLine("Gracias por jugar. ¡Hasta la próxima!");
+        Console.ResetColor();
+    }
+
+    static void PlaySound(string soundFileName)
+    {
+        try
+        {
+            using (var player = new SoundPlayer(soundFileName))
+            {
+                player.Load();
+                player.Play();
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Error al reproducir el sonido: " + ex.Message);
+            Console.ResetColor();
+        }
+    }
+}
---
+++ Parcial_3/Parcial_3/SoundPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.InteropServices;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+class SoundPlayer
+{
+    public SoundPlayer()
+    {
+    }
+
+    internal void Load()
+    {
+        throw new NotImplementedException();
+    }
+
+    internal void Play()
+    {
+        throw new NotImplementedException();
+    }
+}
